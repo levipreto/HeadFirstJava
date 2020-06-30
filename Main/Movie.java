461dabeef2db397fb44ceb8870d8bda6961526d0
Levi Preto <levi.preto@gmail.com>
2020-06-30 14:31:41 -0300

    commit on master

---
+++ Main/Movie.java
@@ -0,0 +1,30 @@
+class Movie {
+    String title;
+    String genre;
+    int rating;
+
+    void playIt() {
+        System.out.println("Playing the movie");
+    }
+}
+
+class MovieTestDrive {
+
+    public static void main(String[] args) {
+        Movie one = new Movie();
+        one.title = "Gone with the Stock";
+        one.genre = "Tragic";
+        one.rating = -2;
+        Movie two = new Movie();
+        two.title = "Lost in cubicle Space";
+        two.genre = "Comedy";
+        two.rating = 5;
+        two.playIt();
+        Movie three = new Movie();
+        three.title = "Byte Club";
+        three.genre = "Tragic but ultimatley uplifting";
+        three.rating = 127;
+
+        Math.random();
+    }
+}
