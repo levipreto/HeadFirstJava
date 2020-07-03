b5f1e7f3efb14c08e79d9df4b32f7ebeee598d9d
Levi Preto <levi.preto@gmail.com>
2020-07-03 14:10:20 -0300

    commit to master

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -1,5 +1,12 @@
 public class MultiplicationTable {
 
+    // first method
+    // void printMultiplicationTable(int table) {
+    //    for (int i = 1; i <= 10; i++) {
+    //        System.out.printf("%d * %d = %d", table, i, table * i).println();
+    //    }
+    //}
+
     public static void main (String[] args) {
     int table = 5;
     int multiply;
